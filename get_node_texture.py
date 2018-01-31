c115376f1535ee3f51b7c0756373bc02d4b0c9b5
Stokes <jim159093@gmail.com>
2018-01-31 19:55:51 +0800

    重構

--- get_node_texture.py
+++ get_node_texture.py
@@ -8,79 +8,105 @@ bl_info = {
 }
 
 import bpy
+from bpy.types import Menu, Panel, UIList
 
-class Update_Texture_Editor(bpy.types.Operator):
-    bl_idname = "vw.get_node_texture"
-    bl_label = "UV"
+
+class Manager():
+    @property
+    def active_object(self):
+        return bpy.context.scene.objects.active
     
-    def get_image_node(self) :
-        material = self.get_Material()
-        nodes = material.node_tree.nodes
-        for node in nodes :
-            if node.type == 'TEX_IMAGE' :
-                if  len(node.outputs['Color'].links) > 0 :
-                    base = node.outputs['Color'].links[0].to_node.bl_rna.base
-                    if base == bpy.types.ShaderNode.bl_rna :
-                        return node
+    @property
+    def material(self):
+        if len(self.active_object.material_slots) > 0 :
+            material = self.active_object.material_slots[0].material
+            return material
         return None
-        
-    def get_image_area(self) :
+    @property
+    def texture_nodes(self):
+        nodes = self.material.node_tree.nodes
+        return [node for node in nodes if node.type == 'TEX_IMAGE']
+
+    @property
+    def uv_editor_area(self):
         for area in bpy.context.screen.areas :
             if area.type == 'IMAGE_EDITOR' :
                 return area
         return None
+
+    def set_image(self, area, image_node):
+        area.spaces.active.image = image_node.image
+
     
-    def set_image_area(self,b):
-        material = self.get_Material()
-        if material is not None:
-            image_node = self.get_image_node()
-            if image_node is not None :
-                b.spaces.active.image = image_node.image
-    
-    def get_Material(self) :
-        obj = bpy.context.scene.objects.active
-        if len(obj.material_slots) > 0 :
-            material = obj.material_slots[0].material
-            return material
+class Texture_Manager_Panel(bpy.types.Panel):
+    """Creates a Panel in the scene context of the properties editor"""
+    bl_label = "Texture Manager"
+    bl_space_type = 'IMAGE_EDITOR'
+    bl_region_type = 'UI'
+
+    manager = Manager()
+
+    def draw(self, context):
+        layout = self.layout
+        self.layout.operator("ops.uv_editor_get_uv_0")
+        self.layout.operator("ops.uv_editor_get_uv_1")
+        
+class UVEditor_get_uv_1(bpy.types.Operator):
+    bl_idname = "ops.uv_editor_get_uv_1"
+    bl_label = "Active"
+
+    def get_image_node(self) :
+        material = Texture_Manager_Panel.manager.material
+        active_node = material.node_tree.nodes.active
+        if active_node is not None:
+            if active_node.type == 'TEX_IMAGE':
+                return active_node
         return None
-    """
-    def get_Material_Output(self, material) :
-        nodes = material.node_tree.nodes
-        for node in nodes :
-            if node.type == 'OUTPUT_MATERIAL' :
-                return node
-        return None
-    
-    def get_image_node(self, output_node) :
-        if len(output_node.inputs['Surface'].links) > 0 :
-            from_node = output_node.inputs['Surface'].links[0].from_node
-            if from_node.bl_rna.base == bpy.types.ShaderNode.bl_rna :
-                if len(from_node.inputs[0].links) > 0 :
-                    from_node2 = from_node.inputs[0].links[0].from_node
-                    if from_node2.type == 'TEX_IMAGE' :
-                        return from_node2
-        return None
-   """
-    def set_image(self) :
-        b = self.get_image_area()
-        if b is not None :
-            self.set_image_area(b)
 
     def execute(self, context):
-        self.set_image()
+        area = Texture_Manager_Panel.manager.uv_editor_area
+        texture_node = self.get_image_node()
+        
+        if texture_node is not None:
+            Texture_Manager_Panel.manager.set_image(area,texture_node)
+        return {'FINISHED'}
+
+class UVEditor_get_uv_0(bpy.types.Operator):
+    bl_idname = "ops.uv_editor_get_uv_0"
+    bl_label = "Classic"
+
+    def get_image_node(self) :
+        Tex_nodes = Texture_Manager_Panel.manager.texture_nodes
+        for node in Tex_nodes :
+            if  len(node.outputs['Color'].links) > 0 :
+                base = node.outputs['Color'].links[0].to_node.bl_rna.base
+                if base == bpy.types.ShaderNode.bl_rna :
+                    return node
+        return None
+
+    def execute(self, context):
+        area = Texture_Manager_Panel.manager.uv_editor_area
+        texture_node = self.get_image_node()
+        if texture_node is not None:
+            Texture_Manager_Panel.manager.set_image(area,texture_node)
         return {'FINISHED'}
     
 def draw_func(self, context):
     layout = self.layout
-    layout.operator("vw.get_node_texture")
+    layout.operator("ops.uv_editor_get_uv_0")
     
 def register():
-    bpy.utils.register_class(Update_Texture_Editor)
+    bpy.utils.register_class(UVEditor_get_uv_0)
+    bpy.utils.register_class(UVEditor_get_uv_1)
     bpy.types.IMAGE_HT_header.prepend(draw_func)
+    bpy.utils.register_class(Texture_Manager_Panel)
+
     
 def unregister():
-    bpy.utils.unregister_class(Update_Texture_Editor)
+    bpy.utils.unregister_class(UVEditor_get_uv_0)
+    bpy.utils.unregister_class(UVEditor_get_uv_1)
     bpy.types.IMAGE_HT_header.remove(draw_func)
+    bpy.utils.unregister_class(Texture_Manager_Panel)
 
 if __name__ == "__main__":
     register()  
